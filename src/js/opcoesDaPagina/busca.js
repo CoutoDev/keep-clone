9046f773c45e775e2a413628b11e985c3b103704
Vitor Coutinho <vitor.coutinho@rcaturismo.com.br>
2020-05-02 14:16:15 -0300

    implementacao da busca de cards

--- src/js/opcoesDaPagina/busca.js
+++ src/js/opcoesDaPagina/busca.js
@@ -1,3 +1,23 @@
 ;(function() {
-    
+    const $busca = $('#busca'); 
+
+    $busca.on('input', (infosDoEvento) => {
+        const elementoEmFocoDuranteEvento = infosDoEvento.target;
+        const valorDaBusca = elementoEmFocoDuranteEvento.value;
+        const $cartoes = document.querySelectorAll('.cartao');
+
+        $cartoes.forEach(($cartao) => {
+            const conteudo = $cartao.querySelector('p').textContent;
+            const conteudoNormalizado = $cartao.toLowerCase();
+            const valorDaBuscaNormalizado = valorDaBusca.toLowerCase().trim();
+
+            if(conteudoNormalizado.includes(valorDaBuscaNormalizado)) {
+                $cartao.style.display = 'block';
+                return;
+            }
+            $cartao.style.display = 'none';
+        });
+
+    });
+    $busca.removeClass('no-js');
 })();
